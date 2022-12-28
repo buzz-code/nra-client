5ca4963745b0515afeac5835011f80c3c0d8557f
buzz-code <hadasa.schechter@gmail.com>
2022-12-28 17:05:20 +0000

    add list actions to common list

--- components/CommonList.jsx
+++ components/CommonList.jsx
@@ -1,21 +1,55 @@
-import { List, Datagrid, CreateButton, ExportButton, TopToolbar, BulkExportButton, BulkDeleteButton } from 'react-admin';
+import { useMemo, useContext } from 'react';
+import { List, Datagrid, CreateButton, ExportButton, TopToolbar, BulkDeleteButton, FilterButton, useListContext, useResourceContext, useResourceDefinition, FilterContext } from 'react-admin';
 
-const ListActions = () => (
-    <TopToolbar>
-        <CreateButton />
-        <ExportButton />
-    </TopToolbar>
-);
+const ListActions = (props) => {
+    const {
+        sort,
+        filterValues,
+        exporter,
+        total,
+    } = useListContext(props);
+    const resource = useResourceContext(props);
+    const { hasCreate } = useResourceDefinition(props);
+    const filters = useContext(FilterContext);
+
+    return useMemo(
+        () => (
+            <TopToolbar {...props}>
+                {filters && <FilterButton />}
+                {hasCreate && <CreateButton />}
+                {exporter !== false && (
+                    <ExportButton
+                        disabled={total === 0}
+                        resource={resource}
+                        sort={sort}
+                        filterValues={filterValues}
+                    />
+                )}
+            </TopToolbar>
+        ),
+        /* eslint-disable react-hooks/exhaustive-deps */
+        [
+            resource,
+            filterValues,
+            filters,
+            total,
+            sort,
+            exporter,
+            hasCreate,
+        ]
+    );
+}
 
 const BulkActionButtons = () => (
     <>
+        {/* <BulkExportButton /> */}
         <BulkDeleteButton />
     </>
 );
 
 
 export const CommonList = ({ children, ...props }) => (
-    <List {...props}>
+    <List actions={<ListActions />} {...props}>
         {children}
     </List>
 )
