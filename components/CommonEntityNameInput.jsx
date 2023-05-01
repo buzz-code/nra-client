38eb14de029a05c4fe2d8639ce1a9a9aaaa97dc8
buzz-code <hadasa.schechter@gmail.com>
2023-05-01 08:37:15 +0000

    add allowedEntities to CommonEntityNameInput

--- components/CommonEntityNameInput.jsx
+++ components/CommonEntityNameInput.jsx
@@ -1,9 +1,10 @@
 import { AutocompleteInput, useGetResourceLabel, useResourceDefinitions } from 'react-admin';
 
-export const CommonEntityNameInput = ({ source, disabled }) => {
+export const CommonEntityNameInput = ({ source, disabled, allowedEntities = [] }) => {
     const resources = useResourceDefinitions();
     const getResourceLabel = useGetResourceLabel();
     const resourceChoices = Object.keys(resources)
+        .filter(key => allowedEntities.length === 0 || allowedEntities.includes(key))
         .map(key => ({ id: key, name: getResourceLabel(key) }))
 
     return (
