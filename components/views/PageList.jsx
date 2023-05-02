4de4474437b8bdcdf07e6d0dddc34fa4c952ae00
buzz-code <hadasa.schechter@gmail.com>
2023-05-02 15:15:24 +0000

    add PageList component

---
+++ components/views/PageList.jsx
@@ -0,0 +1,36 @@
+import { List, RichTextField, useListContext } from "react-admin";
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+export default (props) => {
+    return (
+        <List resource="page" emptyWhileLoading hasCreate={false} exporter={false} {...props}>
+            <PagesData />
+        </List>
+    );
+}
+
+const PagesData = ({ ...props }) => {
+    const { data } = useListContext();
+    return (
+        <Stack spacing={2} sx={{ padding: 2 }}>
+            {data.map(page => (
+                <Accordion key={page.id}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${page.id}-content`} id={`${page.id}-header`}                    >
+                        <Typography>{page.description}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            <RichTextField record={page} source='value' />
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+        </Stack>
+    );
+}
