7b9c2d4b1ba7e4a14f778d3b92dc8e5c96bf42c0
buzz-code <hadasa.schechter@gmail.com>
2023-05-16 13:07:17 +0000

    add BulkActionButton

---
+++ components/crudContainers/BulkActionButton.jsx
@@ -0,0 +1,47 @@
+import { Button, useDataProvider, useListContext, useNotify } from 'react-admin';
+import { useMutation } from 'react-query';
+import DownloadingIcon from '@mui/icons-material/Downloading';
+
+export const BulkActionButton = ({ label, icon, name }) => {
+    const dataProvider = useDataProvider();
+    const { selectedIds, onUnselectItems, resource } = useListContext();
+    const notify = useNotify();
+
+    const params = new URLSearchParams({
+        'extra.action': name,
+        'extra.ids': selectedIds
+    });
+    const { mutate, isLoading } = useMutation(
+        () => dataProvider.exec(resource, 'action?' + params, {}, filename)
+            .then(() => {
+                notify('ra.message.action_success');
+                onUnselectItems();
+            })
+            .catch((error) => {
+                notify(
+                    typeof error === 'string'
+                        ? error
+                        : error.message || 'ra.notification.http_error',
+                    {
+                        type: 'error',
+                        messageArgs: {
+                            _:
+                                typeof error === 'string'
+                                    ? error
+                                    : error && error.message
+                                    ? error.message
+                                    : undefined,
+                        },
+                    }
+                );
+            })
+    );
+
+    return (
+        <Button label={label} onClick={() => mutate()} disabled={isLoading}>
+            {isLoading ? loader : icon}
+        </Button>
+    )
+}
+
+const loader = <DownloadingIcon />;
