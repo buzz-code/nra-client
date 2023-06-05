f957e588e54cd91faf11c182ebf2ee9ad7e0910d
buzz-code <hadasa.schechter@gmail.com>
2023-06-05 19:02:19 +0000

    add ability of dynamic filter in CommonReferenceInputFilter

--- components/fields/CommonReferenceInputFilter.jsx
+++ components/fields/CommonReferenceInputFilter.jsx
@@ -1,9 +1,26 @@
-import { ReferenceInput, AutocompleteInput } from 'react-admin';
+import { useMemo } from 'react';
+import { ReferenceInput, AutocompleteInput, useListContext } from 'react-admin';
 
 const filterToQuery = searchText => ({ 'name:$contL': searchText });
 
-export const CommonReferenceInputFilter = ({ label, source, reference, alwaysOn }) => (
-    <ReferenceInput label={label} source={source} reference={reference} alwaysOn={alwaysOn}>
-        <AutocompleteInput filterToQuery={filterToQuery} />
-    </ReferenceInput>
-);
+export const CommonReferenceInputFilter = ({ label, source, reference, alwaysOn, dynamicFilter }) => {
+    const { filterValues } = useListContext();
+    const filter = useMemo(() => {
+        const res = {};
+        for (const key in dynamicFilter) {
+            if (Object.hasOwnProperty.call(dynamicFilter, key)) {
+                const value = filterValues[key];
+                if (value) {
+                    res[key] = value;
+                }
+            }
+        }
+        return res;
+    }, [dynamicFilter, filterValues]);
+
+    return (
+        <ReferenceInput label={label} source={source} reference={reference} alwaysOn={alwaysOn} filter={filter}>
+            <AutocompleteInput label={label} filterToQuery={filterToQuery} />
+        </ReferenceInput>
+    );
+};
