c479f38d6226fcbf3500bc53f9c91532f000ccd7
buzz-code <hadasa.schechter@gmail.com>
2023-06-13 18:08:23 +0000

    add dynamic filter to CommonReferenceInput

--- components/fields/CommonReferenceInput.tsx
+++ components/fields/CommonReferenceInput.tsx
@@ -1,8 +1,33 @@
 import * as React from 'react';
-import { AutocompleteInput, AutocompleteInputProps, ReferenceInput, ReferenceInputProps } from 'react-admin';
+import { AutocompleteInput, AutocompleteInputProps, ReferenceInput, ReferenceInputProps, FormDataConsumer } from 'react-admin';
 
-export default (props: ReferenceInputProps | AutocompleteInputProps) => (
-    <ReferenceInput {...props}>
-        <AutocompleteInput {...props} />
-    </ReferenceInput>
-)
+type CommonReferenceInputProps = ReferenceInputProps & AutocompleteInputProps & {
+    dynamicFilter: Record<string, string>;
+};
+
+export default (props: CommonReferenceInputProps) => {
+    const getFilterByFormData = React.useCallback((formData) => {
+        const res = {};
+        for (const key in props.dynamicFilter) {
+            if (Object.hasOwnProperty.call(props.dynamicFilter, key)) {
+                const value = formData[key];
+                if (value) {
+                    res[key] = value;
+                }
+            }
+        }
+        return {
+            ...(props.filter as any || {}),
+            ...res,
+        };;
+    }, [props.dynamicFilter, props.filter]);
+
+    return (
+        <FormDataConsumer>
+            {({ formData, ...rest }) => (
+                <ReferenceInput {...props} filter={getFilterByFormData(formData)}>
+                    <AutocompleteInput {...props} />
+                </ReferenceInput>
+            )}</FormDataConsumer>
+    );
+}
