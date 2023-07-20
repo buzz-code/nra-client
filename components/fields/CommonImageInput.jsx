71f43d181adc861e53af7955febd543fc582f8dc
buzz-code <hadasa.schechter@gmail.com>
2023-07-20 12:55:58 +0000

    add CommonImageInput component

---
+++ components/fields/CommonImageInput.jsx
@@ -0,0 +1,28 @@
+import { ImageInput, ImageField } from "react-admin";
+import { useFormContext } from 'react-hook-form';
+import { useEffect } from 'react';
+
+export const CommonImageInput = ({ source, accept = 'image/*', maxSize = 16000000, ...props }) => {
+    const { watch, setValue, setError } = useFormContext();
+    const value = watch(source);
+
+    useEffect(() => {
+        if (value?.src?.includes('blob')) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setValue(source, { ...value, src: reader.result });
+            };
+            reader.onerror = (err) => {
+                setError(source, err);
+            };
+
+            reader.readAsDataURL(value.rawFile);
+        };
+    }, [value, setValue]);
+
+    return (
+        <ImageInput source={source} accept={accept} maxSize={maxSize} {...props}>
+            <ImageField source="src" title="title" />
+        </ImageInput>
+    );
+}
