3fb5fbd4cc0eeb6c3d627b89079e1e58cd770022
buzz-code <hadasa.schechter@gmail.com>
2023-07-20 12:56:06 +0000

    add image table

---
+++ components/common-entities/image.jsx
@@ -0,0 +1,53 @@
+import { AutocompleteInput, DateField, DateTimeInput, ImageField, ImageInput, maxLength, ReferenceField, ReferenceInput, required, TextField, TextInput, useInput, useUnique } from 'react-admin';
+import { CommonDatagrid } from '@shared/components/crudContainers/CommonList';
+import { CommonRepresentation } from '@shared/components/CommonRepresentation';
+import { getResourceComponents } from '@shared/components/crudContainers/CommonEntity';
+import CommonReferenceInput from '@shared/components/fields/CommonReferenceInput';
+import { CommonJsonField, CommonJsonInput } from '@shared/components/fields/CommonJsonItem';
+import { CommonImageInput } from '@shared/components/fields/CommonImageInput';
+
+const filters = [
+    ({ isAdmin }) => isAdmin && <ReferenceInput source="userId" reference="user" />,
+];
+
+const Datagrid = ({ isAdmin, ...props }) => {
+    return (
+        <CommonDatagrid {...props}>
+            {isAdmin && <TextField source="id" />}
+            {isAdmin && <ReferenceField source="userId" reference="user" />}
+            {/* <CommonJsonField source="fileData" /> */}
+            <ImageField source="fileData.src" title="fileData.title" />
+            <TextField source="imageTarget" />
+            {isAdmin && <DateField showDate showTime source="createdAt" />}
+            {isAdmin && <DateField showDate showTime source="updatedAt" />}
+        </CommonDatagrid>
+    );
+}
+
+const imageTargetEnum = ['לוגו לתעודה'];
+
+const Inputs = ({ isCreate, isAdmin }) => {
+    const unique = useUnique();
+
+    return <>
+        {!isCreate && isAdmin && <TextInput source="id" disabled />}
+        {isAdmin && <CommonReferenceInput source="userId" reference="user" validate={required()} />}
+        {/* <CommonJsonInput source="fileData" /> */}
+        <CommonImageInput source="fileData" validate={required()} />
+        <AutocompleteInput source="imageTarget" choices={imageTargetEnum.map(item => ({ id: item, name: item }))} validate={[required(), maxLength(255), isCreate && unique()]} />
+        {!isCreate && isAdmin && <DateTimeInput source="createdAt" disabled />}
+        {!isCreate && isAdmin && <DateTimeInput source="updatedAt" disabled />}
+    </>;
+}
+
+const Representation = CommonRepresentation;
+
+const entity = {
+    Datagrid,
+    Inputs,
+    Representation,
+    filters,
+    exporter: false,
+};
+
+export default getResourceComponents(entity);
