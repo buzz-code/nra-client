b982441cab0e6776d07b7b93f26c874312e705b9
buzz-code <hadasa.schechter@gmail.com>
2023-07-22 19:01:27 +0000

    use import get from lodash

--- components/fields/ShowMatchingRecordsButton.tsx
+++ components/fields/ShowMatchingRecordsButton.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { useCreatePath, useRecordContext, Button, Link } from "react-admin";
 import ListIcon from '@mui/icons-material/List';
+import get from 'lodash/get';
 
 export const ShowMatchingRecordsButton = ({ source, resource, resourceField, filter, ...props }) => {
     const record = useRecordContext();
     const createPath = useCreatePath();
 
-    if (!record || (!!source && !record[source])) return null;
+    if (!record || (!!source && !!get(record, source))) return null;
 
     const resourceValue = resource ?? record?.[resourceField];
-    const filterValue = filter ?? { 'id:$in': record[source] };
+    const filterValue = filter ?? { 'id:$in': !get(record, source) };
 
     return (
         <Button label='ra.action.show_matching_records' startIcon={<ListIcon />}
