cd385dd37eddc92029169ad116838e580b937c30
buzz-code <hadasa.schechter@gmail.com>
2023-08-24 14:48:33 +0000

    update default api url logic

--- providers/constantsProvider.js
+++ providers/constantsProvider.js
@@ -1,1 +1,13 @@
-export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+function getDefaultApiUrl() {
+  const { hostname, port, protocol } = window.location;
+
+  const ipAddressRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+  if (hostname === 'localhost' || ipAddressRegex.test(hostname)) {
+    const nextPort = port ? 8080 : (+port + 1);
+    return `${protocol}//${hostname}:${nextPort}`;
+  }
+
+  return `${protocol}//api.${hostname}`;
+}
+
+export const apiUrl = process.env.REACT_APP_API_URL || getDefaultApiUrl();
