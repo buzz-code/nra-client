9a00c22820130ac5415faa01a6d31e6ef28b50c3
buzz-code <hadasa.schechter@gmail.com>
2023-09-05 17:14:08 +0000

    add MultiReferenceArrayField component

--- components/fields/CommonReferenceField.jsx
+++ components/fields/CommonReferenceField.jsx
@@ -1,4 +1,4 @@
-import { ReferenceField, ReferenceOneField, useRecordContext } from 'react-admin';
+import { ChipField, ListBase, ReferenceArrayField, ReferenceField, ReferenceOneField, SingleFieldList, useRecordContext } from 'react-admin';
 import get from 'lodash/get';
 
 export const CommonReferenceField = ({ source, reference, target, ...props }) => (
@@ -19,6 +19,32 @@ export const MultiReferenceField = ({ source, reference, optionalSource, optiona
     if (record[optionalSource]) {
         return (
             <ReferenceOneField source={optionalSource} reference={reference} target={optionalTarget} sortBy={sortBy} {...props} />
+        )
+    }
+
+    return null;
+}
+
+export const MultiReferenceArrayField = ({ source, reference, optionalSource, optionalTarget, ...props }) => {
+    const record = useRecordContext();
+    if (!record) {
+        return null;
+    }
+
+    if (get(record, source)) {
+        return (
+            <ReferenceArrayField source="klassReferenceIds" reference="klass" {...props} />
+        )
+    }
+
+    const optionalValue = get(record, optionalSource)
+    if (optionalValue) {
+        return (
+            <ListBase resource={reference} filter={{ [optionalTarget]: optionalValue }}>
+                <SingleFieldList linkType={false}>
+                    <ChipField source="name" size="small" />
+                </SingleFieldList>
+            </ListBase>
         )
     }
 
