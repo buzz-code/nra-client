93738bef8479e3ee1431ab6dc857d612f543fbc1
buzz-code <hadasa.schechter@gmail.com>
2023-10-18 19:50:24 +0000

    add permissions to layout custom menu items

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,11 +1,12 @@
-import { Layout } from 'react-admin';
+import { Layout, usePermissions } from 'react-admin';
 import CustomMenu from '@shared/components/layout/Menu';
 import { useIsAdmin } from '@shared/utils/permissionsUtil';
 import { filterArrayByParams } from '@shared/utils/filtersUtil';
 
 const CustomLayout = ({ customMenuItems, menuGroups, ...props }) => {
     const isAdmin = useIsAdmin();
-    const customMenuItemsArr = filterArrayByParams(customMenuItems, { isAdmin });
+    const { permissions } = usePermissions();
+    const customMenuItemsArr = filterArrayByParams(customMenuItems, { isAdmin, permissions });
 
     const Menu = (props) => (
         <CustomMenu {...props} menuGroups={menuGroups}>
