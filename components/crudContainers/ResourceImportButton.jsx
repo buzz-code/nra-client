4c8c628b35563db371109a2b3c022d079c774d2d
buzz-code <hadasa.schechter@gmail.com>
2023-12-05 20:18:59 +0000

    split PreviewListWithSavingDialog component

--- components/crudContainers/ResourceImportButton.jsx
+++ components/crudContainers/ResourceImportButton.jsx
@@ -1,40 +1,11 @@
 import { useState, useCallback } from 'react';
-import { useNotify } from 'react-admin';
-import { useMutation } from 'react-query';
-import { handleError } from '@shared/utils/notifyUtil';
-import { PreviewListDialog } from '../import/PreviewListDialog';
 import { useSavableData } from '../import/util';
 import { ImportButton } from '../import/ImportButton';
-import { useIsAdmin } from "@shared/utils/permissionsUtil";
 
 export const ResourceImportButton = ({ resource, refetch = null, fields, handleDataBeforePreview = null, xlsxOptions, datagrid, handleSuccess = null, ...props }) => {
     const [uploadedData, setUploadedData] = useState(null);
     const [fileName, setFileName] = useState(null);
-    const [tryAgain, setTryAgain] = useState(false);
     const { data, saveData } = useSavableData(resource, fileName, uploadedData);
-    const notify = useNotify();
-    const isAdmin = useIsAdmin();
-
-    const { mutate, isLoading } = useMutation({
-        mutationFn: () => saveData(),
-        onSuccess: ({ successCount, errorCount }) => {
-            if (errorCount === 0 && successCount > 0) {
-                setUploadedData(null);
-                setFileName(null);
-
-                notify('ra.message.import_success', { type: 'info' });
-                refetch?.();
-                handleSuccess?.();
-            } else if (successCount > 0) {
-                notify('ra.message.import_partial_error', { type: 'warning' });
-                refetch?.();
-                setTryAgain(true);
-            } else {
-                notify('ra.message.import_error', { type: 'error' });
-            }
-        },
-        onError: handleError(notify)
-    });
 
     const handleDataParse = useCallback(async ({ name, data }) => {
         const dataToSave = handleDataBeforePreview ? await handleDataBeforePreview(data) : data;
@@ -42,23 +13,15 @@ export const ResourceImportButton = ({ resource, refetch = null, fields, handleD
         setFileName(name);
     }, [setUploadedData, setFileName]);
 
-    const handlePreviewClose = useCallback((isSave, userId) => {
-        if (isSave) {
-            if (isAdmin && userId) {
-                uploadedData.forEach(item => {
-                    item.userId = userId;
-                });
-            }
-            mutate();
-        } else {
-            setUploadedData(null);
-            setFileName(null);
-        }
-    }, [uploadedData, mutate, setUploadedData, setFileName]);
+    const handlePreviewCancel = useCallback(() => {
+        setUploadedData(null);
+        setFileName(null);
+    }, [setUploadedData, setFileName]);
 
     return <>
         <ImportButton fields={fields} handleDataParse={handleDataParse} xlsxOptions={xlsxOptions} {...props} />
-        <PreviewListDialog resource={resource} data={data} isLoading={isLoading}
-            tryAgain={tryAgain} datagrid={datagrid} onDialogClose={handlePreviewClose} />
+        <PreviewListWithSavingDialog resource={resource} datagrid={datagrid}
+            data={data} saveData={saveData}
+            refetch={refetch} handleSuccess={handleSuccess} handlePreviewCancel={handlePreviewCancel} />
     </>
 }
---
+++ components/import/PreviewListWithSavingDialog.tsx
@@ -0,0 +1,50 @@
+import { useState, useCallback } from 'react';
+import { useNotify } from 'react-admin';
+import { useMutation } from 'react-query';
+import { handleError } from '@shared/utils/notifyUtil';
+import { useIsAdmin } from "@shared/utils/permissionsUtil";
+import { PreviewListDialog } from './PreviewListDialog';
+
+export const PreviewListWithSavingDialog = ({ resource, datagrid, data, saveData, refetch, handleSuccess, handlePreviewCancel }) => {
+    const isAdmin = useIsAdmin();
+    const [tryAgain, setTryAgain] = useState(false);
+    const notify = useNotify();
+
+    const { mutate, isLoading } = useMutation({
+        mutationFn: () => saveData(),
+        onSuccess: ({ successCount, errorCount }) => {
+            if (errorCount === 0 && successCount > 0) {
+                handlePreviewCancel();
+
+                notify('ra.message.import_success', { type: 'info' });
+                refetch?.();
+                handleSuccess?.();
+            } else if (successCount > 0) {
+                notify('ra.message.import_partial_error', { type: 'warning' });
+                refetch?.();
+                setTryAgain(true);
+            } else {
+                notify('ra.message.import_error', { type: 'error' });
+            }
+        },
+        onError: handleError(notify)
+    });
+
+    const handlePreviewClose = useCallback((isSave, userId) => {
+        if (isSave) {
+            if (isAdmin && userId) {
+                data.forEach(item => {
+                    item.userId = userId;
+                });
+            }
+            mutate();
+        } else {
+            handlePreviewCancel();
+        }
+    }, [isAdmin, data, mutate, handlePreviewCancel]);
+
+    return (
+        <PreviewListDialog resource={resource} data={data} isLoading={isLoading}
+            tryAgain={tryAgain} datagrid={datagrid} onDialogClose={handlePreviewClose} />
+    );
+};
