4f8c8a058ff49eb74c30faf57eaf6c20dfc2207c
buzz-code <hadasa.schechter@gmail.com>
2024-05-06 19:25:26 +0000

    add excel file parser to file utils

--- components/import/ExcelImportInput.tsx
+++ components/import/ExcelImportInput.tsx
@@ -1,24 +1,15 @@
+import { readAsExcel } from "@shared/utils/fileUtil";
 import React, { forwardRef, useCallback } from "react";
 import * as XLSX from 'xlsx';
 
 export const ExcelImportInput = forwardRef<HTMLInputElement, any>(({ fields, onDataParsed, xlsxOptions = {} }, ref) => {
     const processFile = useCallback(file => {
         var name = file.name;
-        const reader = new FileReader();
-        reader.onload = (evt) => { // evt = on_file_select event
-            /* Parse data */
-            const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, { type: 'binary', cellText: false, cellDates: true });
-            /* Get first worksheet */
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_json(ws, { header: fields, range: 1, ...xlsxOptions });
-            fixDates(data);
-            /* Update state */
-            onDataParsed({ name, data });
-        };
-        reader.readAsBinaryString(file);
+        readAsExcel(file, fields, xlsxOptions)
+            .then(data => {
+                fixDates(data);
+                onDataParsed({ name, data });
+            });
     }, [fields, onDataParsed]);
 
     const handleFileUpload = useCallback(e => {
--- utils/fileUtil.ts
+++ utils/fileUtil.ts
@@ -1,9 +1,29 @@
-export const readAsDataURL = (file: File) => {
+import * as XLSX from 'xlsx';
+
+export const readAsDataURL = (file: File): Promise<string> => {
     const reader = new FileReader();
     return new Promise((resolve, reject) => {
-        reader.onload = () => resolve(reader.result);
+        reader.onload = (event) => resolve((event.target.result) as string);
         reader.onerror = (err) => reject(err);
         reader.readAsDataURL(file);
     });
 };
 
+export const readAsBinaryString = (file: File): Promise<string> => {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+        reader.onload = (event) => resolve((event.target.result) as string);
+        reader.onerror = (err) => reject(err);
+        reader.readAsBinaryString(file);
+    });
+};
+
+export const readAsExcel = (file: File, fields: string[], xlsxOptions = {}) => {
+    return readAsBinaryString(file)
+        .then(bstr => {
+            const workbook = XLSX.read(bstr, { type: 'binary', cellText: false, cellDates: true });
+            const firstSheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[firstSheetName];
+            return XLSX.utils.sheet_to_json(worksheet, { header: fields, range: 1, ...xlsxOptions });
+        });
+}
