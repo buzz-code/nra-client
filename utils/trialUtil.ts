62815205f7edde117c71b5b209971ae1588107e1
buzz-code <hadasa.schechter@gmail.com>
2024-11-06 06:32:34 +0000

    handle case when additionalData is null

--- utils/trialUtil.ts
+++ utils/trialUtil.ts
@@ -33,21 +33,21 @@ export function getTrialAlert(permissions: any, user: any): { type: AlertColor, 
 }
 
 function getTrialMessage(user: any, leftDays: number): string {
-    if (user.additionalData.customTrialMessage) {
+    if (user.additionalData?.customTrialMessage) {
         return user.additionalData.customTrialMessage.replace('{0}', leftDays);
     }
     return `חשבון חינמי, ייסגר בעוד ${leftDays} ימים`;
 }
 
 function getTrialEndedMessage(user: any): string {
-    if (user.additionalData.customTrialEndedMessage) {
+    if (user.additionalData?.customTrialEndedMessage) {
         return user.additionalData.customTrialEndedMessage;
     }
     return 'חשבונך נסגר עקב חוסר תשלום';
 }
 
 function isTrialEnded(user: any): boolean {
-    if (user.additionalData.trialEndDate) {
+    if (user.additionalData?.trialEndDate) {
         const trialEndDate = new Date(user.additionalData.trialEndDate);
         return trialEndDate < new Date();
     }
@@ -55,7 +55,7 @@ function isTrialEnded(user: any): boolean {
 }
 
 function getLeftDays(user: any): number {
-    if (user.additionalData.trialEndDate) {
+    if (user.additionalData?.trialEndDate) {
         const trialEndDate = new Date(user.additionalData.trialEndDate);
         const leftMillis = trialEndDate.getTime() - new Date().getTime();
         return Math.ceil(leftMillis / (1000 * 60 * 60 * 24));
