ba03a574a23ceb84090f69321c0e04d8eff768b8
buzz-code <hadasa.schechter@gmail.com>
2024-11-24 16:22:22 +0000

    add: create BulkFixReferenceButton component for fixing references

---
+++ components/crudContainers/BulkFixReferenceButton.jsx
@@ -0,0 +1,6 @@
+import HelpIcon from '@mui/icons-material/Help';
+import { BulkActionButton } from './BulkActionButton';
+
+export const BulkFixReferenceButton = () => (
+    <BulkActionButton name='fixReferences' label='תיקון שיוך תלמידה' icon={<HelpIcon />} />
+);
