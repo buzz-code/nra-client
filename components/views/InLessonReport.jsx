158eed9c57696b53d7f3dd7aa1080cf2a3b4c2d6
buzz-code <hadasa.schechter@gmail.com>
2024-12-09 18:24:44 +0000

    feat: refactor InLessonReport to use entity configuration for improved resource management

--- components/views/InLessonReport.jsx
+++ components/views/InLessonReport.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useRedirect } from 'react-admin';
 import { useSavableData } from '../import/util';
 import { Datagrid as AttDatagrid } from 'src/entities/att-report';
@@ -6,24 +6,34 @@ import { Datagrid as GradeDatagrid } from 'src/entities/grade';
 import { useIsInLessonReportWithLate } from '@shared/utils/permissionsUtil';
 import { InLessonReport } from '../in-lesson-report';
 
-const attResource = 'att_report';
-const gradeResource = 'grade';
+const entityConfig = [
+    {
+        index: 0,
+        entityLabel: 'נוכחות',
+        resource: 'att_report',
+        Datagrid: AttDatagrid,
+        redirectUrl: '/att_report_with_report_month',
+    },
+    {
+        index: 1,
+        entityLabel: 'ציון',
+        resource: 'grade',
+        Datagrid: GradeDatagrid,
+        redirectUrl: '/grade',
+    },
+];
 
 export default ({ gradeMode = false }) => {
-    const fileContext = gradeMode ? 'ציון' : 'נוכחות';
-    const fileName = useMemo(() => 'דיווח ' + fileContext + ' ' + new Date().toISOString().split('T')[0], []);
     const redirect = useRedirect();
     const isShowLate = useIsInLessonReportWithLate();
-
-    const resource = gradeMode ? gradeResource : attResource;
-    const Datagrid = gradeMode ? GradeDatagrid : AttDatagrid;
+    const { entityLabel, resource, Datagrid, redirectUrl } = useMemo(() => entityConfig[gradeMode ? 1 : 0], [gradeMode]);
+    const fileName = useMemo(() => 'דיווח ' + entityLabel + ' ' + new Date().toISOString().split('T')[0], [entityLabel]);
     const [dataToSave, setDataToSave] = useState(null);
     const { data, saveData } = useSavableData(resource, fileName, dataToSave);
 
-    const handleSuccess = () => {
-        const redirectUrl = gradeMode ? '/grade' : '/att_report_with_report_month';
+    const handleSuccess = useCallback(() => {
         redirect(redirectUrl);
-    };
+    }, [redirect, redirectUrl]);
 
     return (
         <InLessonReport
