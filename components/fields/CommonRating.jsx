4ae6d23b10de19982c4cc218dec0b930ae610bd9
buzz-code <hadasa.schechter@gmail.com>
2025-09-30 17:52:31 +0000

    fix: improve error handling in CommonRatingInput component

--- components/fields/CommonRating.jsx
+++ components/fields/CommonRating.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Rating, FormControl, FormLabel, FormHelperText, Box } from '@mui/material';
 import { get } from 'lodash';
-import { useInput, useRecordContext, useTranslate } from 'react-admin';
+import { InputHelperText, useInput, useRecordContext, useTranslate } from 'react-admin';
 
 const defaultMaxRating = 3;
 
 export const CommonRatingField = ({ source, max = defaultMaxRating, ...props }) => {
   const record = useRecordContext();
   const value = get(record, source, null);
-  
+
   return (
     <Rating
       value={value}
@@ -22,12 +22,12 @@ export const CommonRatingField = ({ source, max = defaultMaxRating, ...props }) 
 export const CommonRatingInput = ({ source, label, max = defaultMaxRating, ...props }) => {
   const {
     field: { value, onChange },
-    fieldState: { error }
+    fieldState: { error, invalid }
   } = useInput({ source });
-  
+
   const translate = useTranslate();
   const finalLabel = label || translate(`resources.event.fields.${source}`, { _: source });
-  
+
   return (
     <FormControl error={!!error} margin="normal">
       <FormLabel component="legend">{finalLabel}</FormLabel>
@@ -41,7 +41,11 @@ export const CommonRatingInput = ({ source, label, max = defaultMaxRating, ...pr
           {...props}
         />
       </Box>
-      {error && <FormHelperText>{error.message}</FormHelperText>}
+      {invalid && (
+        <FormHelperText error={invalid}>
+          <InputHelperText error={error?.message} />
+        </FormHelperText>
+      )}
     </FormControl>
   );
 };
