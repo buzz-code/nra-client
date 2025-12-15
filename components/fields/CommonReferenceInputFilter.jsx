28e0458285156e16cb907a6116e6284923e2c429
buzz-code <hadasa.schechter@gmail.com>
2025-12-15 09:18:03 +0000

    fix: add disabled prop to CommonReferenceInputFilter component

--- components/fields/CommonReferenceInputFilter.jsx
+++ components/fields/CommonReferenceInputFilter.jsx
@@ -3,13 +3,13 @@ import { useMemo } from 'react';
 import { ReferenceInput, useListContext } from 'react-admin';
 import CommonAutocompleteInput from './CommonAutocompleteInput';
 
-export const CommonReferenceInputFilter = ({ label = undefined, source, reference, alwaysOn = {}, dynamicFilter = {}, sort = defaultSortBy }) => {
+export const CommonReferenceInputFilter = ({ label = undefined, source, reference, alwaysOn = {}, dynamicFilter = {}, sort = defaultSortBy, disabled = false }) => {
     const { filterValues } = useListContext();
     const filter = useMemo(() => getDynamicFilter(dynamicFilter, filterValues), [dynamicFilter, filterValues]);
 
     return (
-        <ReferenceInput label={label} source={source} reference={reference} alwaysOn={alwaysOn} filter={filter} sort={sort}>
-            <CommonAutocompleteInput label={label} />
+        <ReferenceInput label={label} source={source} reference={reference} alwaysOn={alwaysOn} filter={filter} sort={sort} disabled={disabled}>
+            <CommonAutocompleteInput label={label} disabled={disabled} />
         </ReferenceInput>
     );
 };
